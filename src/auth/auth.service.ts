8a49e9a7dd4cc9684d52215242264fbbfacebbc1
Nelson Cadenas <nca1478@yahoo.com>
2024-09-02 21:28:59 -0400

    Registro de un usuario

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { RegisterUserDto } from './dto';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class AuthService extends PrismaClient implements OnModuleInit {
@@ -8,5 +10,42 @@ export class AuthService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
     this.logger.log('MongoDB Connected');
+  }
+
+  async registerUser(registerUserDto: RegisterUserDto) {
+    const { email, name, password } = registerUserDto;
+
+    try {
+      const user = await this.user.findUnique({
+        where: {
+          email,
+        },
+      });
+
+      if (user) {
+        throw new RpcException({
+          status: 400,
+          message: 'User already exists',
+        });
+      }
+
+      const newUser = await this.user.create({
+        data: {
+          email,
+          password,
+          name,
+        },
+      });
+
+      return {
+        user: newUser,
+        toke: 'ABC123',
+      };
+    } catch (error) {
+      throw new RpcException({
+        status: 400,
+        message: error.message,
+      });
+    }
   }
 }
